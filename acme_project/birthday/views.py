1632a1cd91a07fad33b9a4dae91c308613e8e01b
Artem <artni96@yandex.ru>
2024-07-05 15:06:02 +0300

    lesson 7 done

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 
 def birthday(request):
-    context = {}
+    form = BirthdayForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update(
+            {'birthday_countdown': birthday_countdown}
+        )
     return render(request, 'birthday/birthday.html', context=context)
+
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {
+        'birthdays': birthdays
+    }
+    return render(request, 'birthday/birthday_list.html', context)
